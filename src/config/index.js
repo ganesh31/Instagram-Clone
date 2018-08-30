2ca342851b8cb591061929ba7a191b5ff71315a7
Ganesh <ganesh.g3105@gmail.com>
2018-08-31 01:42:24 +0530

    Initial Commit with baasic view

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+  images: {
+    arrowIcon: require("../../assets/arrow.png"),
+    heartIcon: require("../../assets/heart.png"),
+    bubbleIcon: require("../../assets/speach-bubble.png")
+  },
+  styleConstants: {
+    rowHeight: 50
+  }
+};
